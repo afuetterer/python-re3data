37ecb9a61aca0ba9ef40836b83a63237776689e5
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2024-01-30 12:48:06 +0100

    feat: add initial cli

--- src/re3data/__init__.py
+++ src/re3data/__init__.py
@@ -3,3 +3,9 @@
 # SPDX-License-Identifier: MIT
 
 """python-re3data."""
+
+from re3data.__about__ import __version__
+
+__all__ = [
+    "__version__",
+]
---
+++ src/re3data/__main__.py
@@ -0,0 +1,5 @@
+"""Entry point for python-re3data."""
+
+from re3data.cli import app
+
+app(prog_name="re3data")
