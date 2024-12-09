9386585624647fbab0dbbd0dcea8d04f691f8109
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2024-12-09 13:37:37 +0100

    s

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -97,18 +97,33 @@ def test_repository_list_json(mock_repository_list_route: Route) -> None:
     assert '"doi": "https://doi.org/10.17616/R3P594",' in result.output
 
 
-def test_repository_list_response(mock_repository_list_route: Route) -> None:
-    result = runner.invoke(app, ["repository", "list", "--return-type", "response"])
-    assert result.exit_code == 0
-    assert "Response" in result.output
-    assert "url=URL('https://www.re3data.org/api/beta/repositories')" in result.output
-
-
 def test_repository_list_dict(mock_repository_list_route: Route) -> None:
     result = runner.invoke(app, ["repository", "list", "--return-type", "dict"])
     assert result.exit_code == 0
     assert "'id': 'r3d100010371'," in result.output
     assert "'doi': 'https://doi.org/10.17616/R3P594'," in result.output
+
+
+def test_repository_list_csv(mock_repository_list_route: Route) -> None:
+    result = runner.invoke(app, ["repository", "list", "--return-type", "csv"])
+    assert result.exit_code == 0
+    assert "id,doi,name,link.href," in result.output
+    assert "https://doi.org/10.17616/R3P594" in result.output
+
+
+def test_repository_list_dataframe(mock_repository_list_route: Route) -> None:
+    result = runner.invoke(app, ["repository", "list", "--return-type", "dataframe"])
+    assert result.exit_code == 0
+    assert "id" in result.output
+    assert "r3d100010371" in result.output
+    assert "[3 rows x 5 columns]" in result.output
+
+
+def test_repository_list_response(mock_repository_list_route: Route) -> None:
+    result = runner.invoke(app, ["repository", "list", "--return-type", "response"])
+    assert result.exit_code == 0
+    assert "Response" in result.output
+    assert "url=URL('https://www.re3data.org/api/beta/repositories')" in result.output
 
 
 def test_repository_list_invalid_return_type(mock_repository_list_route: Route) -> None:
@@ -212,6 +227,22 @@ def test_repository_get_with_repository_id_dict(mock_repository_get_route: Route
     assert result.exit_code == 0
     assert "{" in result.output
     assert "'re3data.orgIdentifier': 'r3d100010468'," in result.output
+
+
+def test_repository_get_with_repository_id_csv(mock_repository_get_route: Route, zenodo_id: str) -> None:
+    result = runner.invoke(app, ["repository", "get", zenodo_id, "--return-type", "csv"])
+    assert result.exit_code == 0
+    assert result.exit_code == 0
+    assert "re3data.orgIdentifier,additionalName,repositoryURL," in result.output
+    assert "r3d100010468" in result.output
+
+
+def test_repository_get_with_repository_id_dataframe(mock_repository_get_route: Route, zenodo_id: str) -> None:
+    result = runner.invoke(app, ["repository", "get", zenodo_id, "--return-type", "dataframe"])
+    assert result.exit_code == 0
+    assert "re3data.orgIdentifier" in result.output
+    assert "r3d100010468" in result.output
+    assert "[1 rows x 43 columns]" in result.output
 
 
 def test_repository_get_with_repository_id_response(mock_repository_get_route: Route, zenodo_id: str) -> None:
